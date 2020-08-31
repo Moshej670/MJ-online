0d9749ddd01ad3872c43ba5826667a045f1d8ce2
Moshej670 <69690754+Moshej670@users.noreply.github.com>
2020-08-31 18:43:35 -0400

    add new 4

---
+++ Saved Pictures - Copy/gred.js
@@ -0,0 +1,43 @@
+﻿function gred()
+
+{
+var nospam = 1; 
+
+var username = prompt(' enter username',' my username');
+var password = prompt('enter password ' ,'my password');
+while( nospam < 3)
+{
+
+
+
+if (username.toLowerCase() === 'moshe' && password.toLowerCase() ==='042404'){
+window.alert('welcome master moshe');
+window.open('beginers website.html');
+}
+
+if (username.toLowerCase() === '' && password.toLowerCase() ===''){
+window.alert('welcome ');
+ window.open('beginers website.html');
+}
+
+if (username.toLowerCase() === '' && password.toLowerCase() ===''){
+window.alert(' ');
+window.open('beginers website.html');
+}
+
+else history.go (-1);
+
+break;
+}
+nospam+=1;
+var username = 
+prompt('Access Denied - username or password Incorrect, Please Try Again.','username');
+var password = 
+prompt('username correct please enter password','password');
+
+}
+/* 
+if (nospam == 3) {
+history.go(-1);
+} 
+*/
