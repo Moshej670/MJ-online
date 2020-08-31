5734ebcfe8ee21be34e1e4bf52b20b0ddcb7148e
Moshej670 <69690754+Moshej670@users.noreply.github.com>
2020-08-31 15:44:08 -0400

    latest updates

---
+++ javascrall.js
@@ -0,0 +1,11 @@
+﻿/*
+function blur () {
+var nav = document.getElementById("modtef");
+var collap = document.getElementById("collapsable-nav");
+nav.blur(function(event) {
+
+collap.collapse('hide');
+
+	)};
+
+};
