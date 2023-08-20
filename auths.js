2a5325c151bc4069075c8f27a4805da1076503ea
Moshej670 <69690754+Moshej670@users.noreply.github.com>
2023-08-19 20:30:52 -0400

    Update auths.js

--- auths.js
+++ auths.js
@@ -10,12 +10,8 @@ const ui = new firebaseui.auth.AuthUI(firebase.auth());
 // Listen for authentication state changes
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    // User is signed in, show content
-
-    document.getElementById('loader').style.display = 'none';
-    document.getElementById('content').style.display = 'block';
-
-    // Set up sign-out button
+    // User is signed in dont do else
+    
     document.getElementById('sign-out-button').addEventListener('click', function() {
       firebase.auth().signOut().then(function() {
         // User is signed out
@@ -25,28 +21,9 @@ firebase.auth().onAuthStateChanged(function(user) {
         console.error('Sign-out error:', error);
       });
     });
-
-    // Set up sign-out and go home button
-    document.getElementById('adminsign1').addEventListener('click', function() {
-      firebase.auth().signOut().then(function() {
-        // User is signed out
-        window.location.href = 'https://moshejanani.com/admin'; // Redirect to sign-in page
-      }).catch(function(error) {
-        // An error happened
-        console.error('Sign-out error:', error);
-      });
-    });
-
-    document.getElementById('adminsign2').addEventListener('click', function() {
-      firebase.auth().signOut().then(function() {
-        // User is signed out
-        window.location.href = 'https://moshejanani.com/'; // Redirect to home page
-      }).catch(function(error) {
-        // An error happened
-        console.error('Sign-out error:', error);
-      });
-    });
-  } else {
+  }
+  
+  else {
     // User is not signed in, redirect to sign-in page
     window.location.href = 'https://moshejanani.com/admin'; // Change this URL
   }
